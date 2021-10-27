124d82f59baea9132a37388b37cc67a95e735a73
kosuha <okeydokekim@gmail.com>
2021-10-27 21:50:03 +0900

    구독하기 페이지

--- client/src/components/views/PaymentPage/PaymentPage.js
+++ client/src/components/views/PaymentPage/PaymentPage.js
@@ -1,20 +1,23 @@
-import React from 'react'
+import React from "react";
+import { DivCardContainer, DivCard } from "./style";
 
 function PaymentPage(props) {
     const planList = ["1", "2"];
     const plan = props.match.params.plan;
-    
+
     if (!planList.includes(plan)) {
         props.history.push("/");
-    } 
+    }
 
     console.log(props);
 
     return (
-        <div>
-            {plan}
-        </div>
-    )
+        <DivCardContainer>
+            <DivCard>
+                {plan}
+            </DivCard>
+        </DivCardContainer>
+    );
 }
 
-export default PaymentPage
+export default PaymentPage;
---
+++ client/src/components/views/PaymentPage/style.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const DivCardContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    margin: 3rem auto;
+`;
+
+export const DivCard = styled.div`
+    width: 20%;
+    height: 25rem;
+    min-width: 20rem;
+    margin: 1rem;
+    padding: 0rem;
+    border-radius: 0.5rem;
+    box-shadow: 0.1rem 0.1rem 1rem 0rem rgb(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+`;
